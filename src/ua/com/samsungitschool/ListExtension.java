913b63f5b8f5b9037e58ade0792ab68f2ea668b4
Yaroslav Polykov <polyakov.production@gmail.com>
2018-12-08 19:55:21 +0300

    Initial commit

---
+++ src/ua/com/samsungitschool/ListExtension.java
@@ -0,0 +1,11 @@
+package ua.com.samsungitschool;
+
+import java.util.List;
+
+public class ListExtension implements IListDeleteByFilter<Integer>{
+
+    @Override
+    public List<Integer> deleteFromList(List<Integer> list, Integer deleteFilterKey) {
+        return null;
+    }
+}
---
+++ src/ua/com/samsungitschool/Main.java
@@ -0,0 +1,8 @@
+package ua.com.samsungitschool;
+
+class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
