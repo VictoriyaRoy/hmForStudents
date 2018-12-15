113638db4650bdc650cb84424d56c66d4074794b
VictoriyaRoi <victoriyaroi2003@gmail.com>
2018-12-15 12:44:06 +0200

    /

--- src/ua/com/samsungitschool/ListExtension.java
+++ src/ua/com/samsungitschool/ListExtension.java
@@ -1,11 +1,17 @@
 package ua.com.samsungitschool;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ListExtension implements IListDeleteByFilter<Integer>{
 
+
     @Override
     public List<Integer> deleteFromList(List<Integer> list, Integer deleteFilterKey) {
-        return null;
+        int count = Collections.frequency(list,deleteFilterKey);
+        for (int i=0; i<count;i++){
+            list.remove(deleteFilterKey);
+        }
+        return list;
     }
 }
